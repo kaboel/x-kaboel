76535561fc2291ddfb5a68be1ee940d8fa54b866
Faiq <faiq.kaboel@gmail.com>
2019-06-24 14:26:53 +0700

    Setting up routes for added methods

--- src/_core/config-sample.js
+++ src/_core/config-sample.js
@@ -1,6 +1,6 @@
 module.exports = {
-    name: 'x-kaboel',
-    port: '8089',
+    name: 'x-kaboel', // Change the name if you want PS: the name doesn't really matter
+    port: '8089', // Change the port if you want
     db: {
         uriString: ''// Your database connection uriString
     }                // e.g `mongodb+srv://<user>:<pass>@<host>/<dbName>?retryWrites=true&w=majority`
--- src/routes.js
+++ src/routes.js
@@ -4,4 +4,6 @@ module.exports = (App) => {
     // Contact Routes
     App.get('/contacts', ContactController.index);
     App.post('/contacts', ContactController.create);
+    App.get('/contacts/contact=:id', ContactController.show);
+    App.delete('/contacts', ContactController.destroy);
 }

645d2bf516f619ecc5f0340b1feb2a425e4e7ca7
Faiq <faiq.kaboel@gmail.com>
2019-06-24 07:05:34 +0700

    Setup entry point app.js

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const config = require('./_config/config');
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+const App = express();
+App.use(morgan('combined'));
+App.use(bodyParser.json());
+App.use(bodyParser.urlencoded({ extended: true }));
+
+try {
+    const port = process.env.PORT || config.port;
+    App.listen(port, () => {
+        console.log(`Express server started on port: ${port}...`);
+    })
+} catch (err) {
+    console.log('An error has occurred - Server fail to start.');
+}
+
+
+

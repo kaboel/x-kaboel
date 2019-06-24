3be67e18b64aa208c3580e29b2fa7d10100abcca
Faiq <faiq.kaboel@gmail.com>
2019-06-24 13:29:59 +0700

    Setting up routes for ContactController methods

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,7 @@
+const ContactController = require('./_controllers/ContactController');
+
 module.exports = (App) => {
-    
+    // Contact Routes
+    App.get('/contacts', ContactController.index);
+    App.post('/contacts', ContactController.create);
 }

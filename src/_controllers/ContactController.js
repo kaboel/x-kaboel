f73cb73d375f3906b9c140bd392726452b298af6
Faiq <faiq.kaboel@gmail.com>
2019-06-24 14:26:39 +0700

    Add methods for findById and delete

--- src/_controllers/ContactController.js
+++ src/_controllers/ContactController.js
@@ -3,12 +3,12 @@ const Contact = require('../_models/Contact');
 module.exports = {
     async index(req, res) {
         try {
-            await Contact.find({}, (err, contacts) => {
-                res.json(contacts);
+            await Contact.find({}, (err, data) => {
+                res.json(data);
             })
         } catch (err) {
             res.status(500).send({
-                error: `An error has occurred - ${err.message}`
+                error: `An error has occurred - ${err.message}.`
             })
         }
     },
@@ -20,9 +20,40 @@ module.exports = {
             res.json(contact);
         } catch (err) {
             res.status(500).send({
-                error: `An error has occurred - ${err.message}`
+                error: `An error has occurred - ${err.message}.`
             })
         }
+    },
+
+    async show(req, res) {
+         await Contact.findById(req.params.id)
+            .then(data => {
+
+                (data !== null) ? res.json(data) : res.send({ error: `Contact not found.`});
+
+            }).catch(err => {
+                res.status(500).send({
+                    error: `An error has occurred - ${err.message}.`
+                })
+            })
+    },
+
+    async destroy(req, res) {
+        await Contact.findById(req.body._id)
+            .then(async data => {
+
+                (data === null) ? res.send({ error: `Unable to delete - Contact not found.`}) :
+
+                await Contact.deleteOne(req.body , (err) => {
+                    if(err) res.status(500).send({ error: `An error has occurred - ${err}.` });
+                    res.send(`Data deleted.`)
+                })
+
+            }).catch(err => {
+                res.status(500).send({
+                    error: `An error has occurred - ${err.message}`
+                })
+            });
     }
 }
 

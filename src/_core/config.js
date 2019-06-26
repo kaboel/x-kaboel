756153ddf07d77ec074b47cf974988fb715c2821
Faiq <faiq.kaboel@gmail.com>
2019-06-27 04:25:32 +0700

    Add config

---
+++ src/_core/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'x-kaboel',
+    port: '8089',
+    db: {
+        uriString: `mongodb+srv://kaboel:Ff456217@kluster-0-co84f.mongodb.net/v-kaboel?retryWrites=true&w=majority`
+    }
+}

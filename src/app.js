7197c7b67753cb9e837c9a8126dc752a309a2265
Faiq <faiq.kaboel@gmail.com>
2019-06-27 01:51:06 +0700

    Implement Express.route to reduce path redundancy

--- src/app.js
+++ src/app.js
@@ -10,25 +10,23 @@ App.use(morgan('combined'));
 App.use(bodyParser.json());
 App.use(bodyParser.urlencoded({ extended: true }));
 
+// Routes
 require('./routes')(App);
 
 const db = require('./_core/config').db;
-mongoose.connect(db.uriString, { useNewUrlParser: true });
-
-const conn = mongoose.connection;
-conn.once('open', () => {
+mongoose.connect(db.uriString, { useNewUrlParser: true })
+    .catch(err => {
+        console.log(`!![ERROR]: Database failed to connect - ${err.message}`);
+    }).then(() => {
     console.log('Database Connected...');
+
     try {
         const port = process.env.PORT || config.port;
         App.listen(port, () => {
-            console.log(`Express server started on port: ${port}...`);
+            console.log(`Server started on port: ${port}...`);
         })
     } catch (err) {
-        console.log(`ERROR: Server failed to start - ${err.message}`);
+        console.log(`!![ERROR]: Server failed to start - ${err.message}`);
     }
 });
-conn.on('error', (err) => {
-    console.log(`Database connection error - ${err}`);
-});
-
 
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,22 @@
 const ContactController = require('./_controllers/ContactController');
+const PlanController = require('./_controllers/PlanController');
 
 module.exports = (App) => {
-    // Contact Routes
-    App.get('/contacts', ContactController.index);
-    App.post('/contacts', ContactController.create);
-    App.get('/contacts/contact=:id', ContactController.show);
-    App.delete('/contacts', ContactController.destroy);
 
-    // Plans Routes
-    
+    // Contact routes
+    App.get('/api/contacts/:id', ContactController.show);
+    App.route('/api/contacts')
+        .get(ContactController.index)
+        .post(ContactController.store)
+        .delete(ContactController.destroy);
+
+    // Plan routes
+    App.get('/api/plans/:id', PlanController.show);
+    App.route('/api/plans')
+        .get(PlanController.index)
+        .post(PlanController.store)
+        .delete(PlanController.destroy);
+
 }
+
 

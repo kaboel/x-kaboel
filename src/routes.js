c729dafb67bb71bd72afdc984aadfdd115d8ccb7
Faiq <faiq.kaboel@gmail.com>
2019-06-27 02:12:30 +0700

    Change declaration name for ContactController and PlanController

--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,21 @@
-const ContactController = require('./_controllers/ContactController');
-const PlanController = require('./_controllers/PlanController');
+const Contact = require('./_controllers/ContactController');
+const Plan = require('./_controllers/PlanController');
 
 module.exports = (App) => {
 
     // Contact routes
-    App.get('/api/contacts/:id', ContactController.show);
+    App.get('/api/contacts/:id', Contact.show);
     App.route('/api/contacts')
-        .get(ContactController.index)
-        .post(ContactController.store)
-        .delete(ContactController.destroy);
+        .get(Contact.index)
+        .post(Contact.store)
+        .delete(Contact.destroy);
 
     // Plan routes
-    App.get('/api/plans/:id', PlanController.show);
+    App.get('/api/plans/:id', Plan.show);
     App.route('/api/plans')
-        .get(PlanController.index)
-        .post(PlanController.store)
-        .delete(PlanController.destroy);
+        .get(Plan.index)
+        .post(Plan.store)
+        .delete(Plan.destroy);
 
 }
 

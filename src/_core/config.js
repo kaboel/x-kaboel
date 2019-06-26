7bee8a2a909f490ee24db1f0a537ce30210cb223
Faiq <faiq.kaboel@gmail.com>
2019-06-27 04:33:49 +0700

    remove config

--- src/_core/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'x-kaboel',
-    port: '8089',
-    db: {
-        uriString: `mongodb+srv://kaboel:Ff456217@kluster-0-co84f.mongodb.net/v-kaboel?retryWrites=true&w=majority`
-    }
-}
